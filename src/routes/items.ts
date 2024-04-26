7fc7a1663dfb06c9744e11ac884c5bbe507f6bc2
Rodrigo <digo.baiocchi@gmail.com>
2024-04-26 15:46:53 -0400

    adding get, post, put and delete requests to item router

--- src/routes/items.ts
+++ src/routes/items.ts
@@ -3,7 +3,12 @@ import { data } from "../controllers/items";
 
 const router:Router = Router();
 
-router.get('/', data.getItem);
+router.get('/', data.getItems);
+
 router.post('/', data.postItem);
+
+router.put('/:id');
+
+router.delete('/:id');
 
 export default router;
