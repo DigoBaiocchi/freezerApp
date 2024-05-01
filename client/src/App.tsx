f2b19b399fdaa6313946a584e0b65cbf37b42b3a
Rodrigo <digo.baiocchi@gmail.com>
2024-04-30 21:50:18 -0400

    installing react-router-dom, creating Home component and add to App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,34 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
+// import Login from './pages/Login/login';
+// import Categories from './pages/Categories/Categories';
+// import Products from './pages/Products/Products';
+import Home from './pages/Home';
+
+import { 
+  RouterProvider,
+  createBrowserRouter, 
+  createRoutesFromElements,
+  Route
+} from 'react-router-dom';
+// import { Provider } from 'react-redux';
+// import { store } from './store/store';
+
+const router = createBrowserRouter(createRoutesFromElements(
+  <>
+    <Route path='/' element={<Home />} />
+    {/* <Route path='/login' element={<Login />} />
+    <Route path='/categories' element={<Categories />} />
+    <Route path='/products' element={<Products />} /> */}
+  </>
+));
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    // <Provider store={store}>
+      <RouterProvider router={router} />
+    // </Provider>
   )
 }
 
