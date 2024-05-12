a8f179055671d1d55561c19fc380c2c67bec8bb7
Rodrigo <digo.baiocchi@gmail.com>
2024-05-11 20:48:39 -0400

    adding last database schema - changing table name from freezer_category_item to inventory

--- src/database/oldDbSchema.ts
+++ src/database/oldDbSchema.ts
@@ -23,3 +23,16 @@ const createFreezerCategoryItemTable = `CREATE TABLE IF NOT EXISTS freezer_categ
     category_id integer REFERENCES category (id) ON DELETE CASCADE,
     item_id integer REFERENCES item (id) ON DELETE CASCADE
 );`;
+
+const createFreezerCategoryItemTableV2 = `CREATE TABLE IF NOT EXISTS freezer_category_item (
+    id uuid DEFAULT gen_random_uuid(),
+    freezer_id uuid REFERENCES freezer (id) ON DELETE CASCADE,
+    category_id uuid REFERENCES category (id) ON DELETE CASCADE,
+    item_id uuid REFERENCES item (id) ON DELETE CASCADE,
+    unit_id uuid REFERENCES unit (id) ON DELETE CASCADE,
+    entry_date date NOT NULL,
+    exp_date date NOT NULL,
+    quantity integer NOT NULL,
+    description varchar(100) DEFAULT '',
+    PRIMARY KEY (id)
+);`;
