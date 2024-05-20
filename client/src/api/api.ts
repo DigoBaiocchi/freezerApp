e0267482c7e707fd89c87a3231965fa657bbbfd9
Rodrigo <digo.baiocchi@gmail.com>
2024-05-20 17:12:55 -0400

    creating get, put and delete api call

--- client/src/api/api.ts
+++ client/src/api/api.ts
@@ -1,18 +1,41 @@
 import axios from "axios";
 
-type IndividualTables = "freezer" | "category" | "item" | 'unit';
+export type IndividualTables = "freezer" | "category" | "item" | 'unit';
 
 export class ApiCalls {
     individualTable: IndividualTables;
+    apiUrl: string;
+
     constructor(individualTable: IndividualTables) {
         this.individualTable = individualTable;
+        this.apiUrl = `http://localhost:3000/${this.individualTable}`;
     }
 
-    postCall(name: string) {
-        axios.post(`http://localhost:3000/${this.individualTable}`, {
+    async postCall(name: string) {
+        axios.post(this.apiUrl, {
                 name: name
               })
               .then(response => console.log(response))
               .catch(error => console.log(error))
+    }
+
+    async getCall() {
+        return axios.get(this.apiUrl)
+            .then(response => response.data)
+            .catch(error => console.log(error))
+    }
+
+    async updateCall(id: string, name: string) {
+        axios.put(`${this.apiUrl}/${id}`, {
+                name: name
+            })
+            .then(response => console.log(response))
+            .catch(error => console.log(error))
+    }
+
+    async deleteCall(id: string) {
+        axios.delete(`${this.apiUrl}/${id}`)
+                .then(response => console.log(response))
+                .catch(error => console.log(error))
     }
 }
