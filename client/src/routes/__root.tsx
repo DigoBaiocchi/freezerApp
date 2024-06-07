9a9670a5f4e680db8741bb3aba0c0f5d013ce6e3
Rodrigo <digo.baiocchi@gmail.com>
2024-06-06 20:41:40 -0400

    installing shadcn navigation-menu and add to _root.tsx

--- client/src/routes/__root.tsx
+++ client/src/routes/__root.tsx
@@ -1,29 +1,42 @@
+import { NavigationMenu, NavigationMenuItem, NavigationMenuLink, navigationMenuTriggerStyle } from "@/components/ui/navigation-menu";
 import { createRootRoute, Link, Outlet } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from '@tanstack/router-devtools'
 
 const Root = () => {
     return (
         <>
-            <div>
-                <Link to="/home">
-                    Home
-                </Link>
-                <Link to="/freezer">
-                    Freezers
-                </Link>
-                <Link to="/category">
-                    Categories
-                </Link>
-                <Link to="/item">
-                    Items
-                </Link>
-                <Link to="/unit">
-                    Units
-                </Link>
-                <Link to="/inventory">
-                    Inventory
-                </Link>
-            </div>
+            <NavigationMenu>
+                <NavigationMenuItem>
+                    <Link to="/home">
+                        <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                            Home
+                        </NavigationMenuLink>
+                    </Link>
+                    <Link to="/freezer">
+                        <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                            Freezers
+                        </NavigationMenuLink>
+                    </Link>
+                    <Link to="/category">
+                        <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                            Categories
+                        </NavigationMenuLink>
+                    </Link>
+                        <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                            Items
+                        </NavigationMenuLink>
+                    <Link to="/unit">
+                        <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                            Units
+                        </NavigationMenuLink>
+                    </Link>
+                    <Link to="/inventory">
+                        <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                            Inventory
+                        </NavigationMenuLink>
+                    </Link>
+                </NavigationMenuItem>
+            </NavigationMenu>
             <hr />
             <Outlet />
             <TanStackRouterDevtools />
