60a033e73903a7620d919c082c5ffaff7597227c
Rodrigo <digo.baiocchi@gmail.com>
2024-09-08 16:07:18 -0400

    add log to checkForMissingRequiredParams util function

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -3,6 +3,6 @@ import { IndividualTables, NonInventoryData, NonInventoryQueries } from "../data
 export const checkForMissingRequiredParams = async (tableName: IndividualTables, id: number) => {
     const database = new NonInventoryQueries(tableName);
     const freezerData = await database.getData() as NonInventoryData[];
-
+    console.log(freezerData);
     return freezerData.map(freezer => freezer.id).includes(id);
 };
