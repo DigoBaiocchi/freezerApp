4dbe54c183449f53f6c32fbcc8f3bd53cd0b999d
Rodrigo <digo.baiocchi@gmail.com>
2024-09-08 16:18:55 -0400

    Converting ids from database to Number
    Eventhough tables have int8 ids, supabase return ids as strings so in
    checkForMissingRequiredParams I added Number() to convert ids back to
    integers

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -2,7 +2,7 @@ import { IndividualTables, NonInventoryData, NonInventoryQueries } from "../data
 
 export const checkForMissingRequiredParams = async (tableName: IndividualTables, id: number) => {
     const database = new NonInventoryQueries(tableName);
-    const freezerData = await database.getData() as NonInventoryData[];
-    console.log(freezerData);
-    return freezerData.map(freezer => freezer.id).includes(id);
+    const databaseData = await database.getData() as NonInventoryData[];
+    console.log(databaseData);
+    return databaseData.map(data => Number(data.id)).includes(id);
 };
